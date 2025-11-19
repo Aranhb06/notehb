061e5eb7fda7fcd1fc149c4858c1acc0af1a9edd
Aranhb06 <aranhb06@gmail.com>
2025-11-19 21:38:48 +0100

    funcion inicial

--- notehb/core.py
+++ notehb/core.py
@@ -1,11 +1,28 @@
 """
 file logic internal 
 """
-# --- Imports ---
+
+""" ------------ MODULOS ------------"""
 import os
 import json
+from os.path import isdir
 import time
 
-# --- Funtions globals ---
-"""Funcion de inicio"""
+""" ------------ FUNCIONES GLOBALES ------------"""
+# --- Funcion de configuracion inicial ---
+def initial_config():
+    # Tarea de testeo
+    test_task_list = [{'name':'test', 'description':'test', 'date':'01/01/3000', 'status':False}]
 
+    # Comprobar no existe notehb lo crea
+    if os.path.isdir('~/.config/notehb') != True:
+        os.mkdir('~/.config/notehb')
+
+    # Comprobar no existe hb_list lo crea
+    if os.path.isdir('~/.config/notehb/hb_list') != True:
+        os.mkdir('~/.config/notehb/hb_list')
+
+    # Comprobar no existe default.json lo crea
+    if os.path.isdir('~/.config/notehb/hb_list/default.json') != True:
+        with open('.config/notehb/hb_list/default.json', 'w', encoding="utf-8") as fileconf:
+            fileconf.write(test_task_list)
