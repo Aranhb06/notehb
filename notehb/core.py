0090fcfee19cd293880d0f73cc06d5f66694745d
Aranhb06 <aranhb06@gmail.com>
2025-11-19 20:06:49 +0100

    Avanzando en la estructura

--- notehb/core.py
+++ notehb/core.py
@@ -2,18 +2,10 @@
 file logic internal 
 """
 # --- Imports ---
-import time
 import os
+import json
+import time
 
 # --- Funtions globals ---
-def init_default(dir_config,list_task):
-    if os.system('ls ~/.config/ | grep notehb') != dir_config:
-        os.system('mkdir ~/.config/notehb')
-    else:
-        if os.system('ls ~/.config/notehb/ | grep list') != dir_config:
-            os.system('mkdir ~/.config/notehb/list')
-        else:
-            if os.system('ls ~/.config/notehb/list | grep default.json') != list_task:
-                os.system('touch ~/.config/notehb/default.json')
-            else:
+"""Funcion de inicio"""
 
--- notehb/main.py
+++ notehb/main.py
@@ -1,3 +1,22 @@
 """
 file config commands
 """
+import typer
+app = typer.Typer()
+
+
+@app.command()
+def (s: str):
+    print(f"Hello {s}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__":
+    app()
